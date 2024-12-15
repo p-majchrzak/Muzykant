0ed933ca6949485f10589d93bde9cb2d3162a750
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-12-15 20:02:21 +0100

    Add project files.

---
+++ Muzykant/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Muzykant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Muzyka> Lista = new List<Muzyka>();
+        int licznik = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Lista = Odczytaj();
+            UstawWyglad(Lista[licznik]);
+        }
+        public void UstawWyglad(Muzyka muzyka)
+        {
+            Nazwa.Content = muzyka.Artysta;
+            Album.Content = muzyka.Album;
+            IloscPobran.Content = muzyka.IloscPobran;
+            IloscUtworow.Content = muzyka.IloscPiosenek + " utworów";
+            Rok.Content = muzyka.Rok;
+        }
+        public List<Muzyka> Odczytaj()
+        {
+            List<Muzyka> lista = new List<Muzyka>();
+            StreamReader sr = new StreamReader("Data.txt");
+            while(!sr.EndOfStream)
+            {
+                Muzyka muzyka = new Muzyka(sr.ReadLine(), sr.ReadLine(), int.Parse(sr.ReadLine()), int.Parse(sr.ReadLine()), int.Parse(sr.ReadLine()));
+                sr.ReadLine(); //skip line
+                lista.Add(muzyka);
+            }
+            return lista;
+        }
+        private void Poprzedni_Click(object sender, RoutedEventArgs e)
+        {
+            licznik--;
+            if(licznik >= 0)
+            {
+                UstawWyglad(Lista[licznik]);
+            }
+            else
+            {
+                licznik = Lista.Count-1;
+                UstawWyglad(Lista[licznik]);
+            }
+        }
+        private void Nastepny_Click(object sender, RoutedEventArgs e)
+        {
+            licznik++;
+            if (licznik < Lista.Count)
+            {
+                UstawWyglad(Lista[licznik]);
+            }
+            else
+            {
+                licznik = 0;
+                UstawWyglad(Lista[licznik]);
+            }
+        }
+        private void Pobierz_Click(object sender, RoutedEventArgs e)
+        {
+            Lista[licznik].IloscPobran++;
+            UstawWyglad(Lista[licznik]);
+        }
+    }
+}
---
+++ Muzykant/Muzyka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muzykant
+{
+    public class Muzyka
+    {
+        public string ?Artysta { get; set; }
+        public string ?Album { get; set; }
+        public int IloscPiosenek { get; set; }
+        public int Rok { get; set; }
+        public int IloscPobran { get; set; }
+        public Muzyka() { }
+        public Muzyka(string? artysta, string? album, int iloscPiosenek, int rok, int iloscPobran)
+        {
+            Artysta = artysta;
+            Album = album;
+            IloscPiosenek = iloscPiosenek;
+            Rok = rok;
+            IloscPobran = iloscPobran;
+        }
+    }
+}
